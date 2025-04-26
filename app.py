940437b329e2ba974617b5a0f980ac4b6992dfc8
Aziz Ahmad <55359922+azizahmad7751@users.noreply.github.com>
2025-04-27 01:43:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,20 @@ import os
 import PyPDF2
 import streamlit as st
 from dotenv import load_dotenv
-from sentence_transformers import SentenceTransformer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.tools import ArxivQueryRun
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from litellm import completion
-from langchain.embeddings import HuggingFaceEmbeddings
-
 
 # Load environment variables
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 huggingface_token = os.getenv("HUGGINGFACE_TOKEN")
-'''
-if huggingface_token:
-    login(token=huggingface_token)
-'''
-
-
 
 # Initialize models and tools
-text_embedding_model = SentenceTransformer('all-MiniLM-L6-v2', device='cpu')
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
 arxiv_tool = ArxivQueryRun()
-
 
 def extract_text_from_pdfs(uploaded_files):
     all_text = ""
